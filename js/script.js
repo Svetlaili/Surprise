0b701d04ecd13c9410ef96e81956daf297d71269
Svetla Ilieva <svetlaili@gmail.com>
2026-07-16 15:50:27 +0300

    Update unlock date to 2026-07-24
    Extend the unlock date from July 16 to July 24, 2026.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const TEST_MODE = false;
 const FAST_MODE = false;
 
-const unlock = new Date("2026-07-16T12:30:00Z");
+const unlock = new Date("2026-07-24T12:30:00Z");
 
 const WAIT = FAST_MODE ? 2500 : 7000;
 const LETTER = FAST_MODE ? 240 : 180;
